e98e431b764e98836eb13dd1799d7ae8e18e7c0f
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-07 12:17:48 -0500

    implement myOr

--- bootcamp-prep-master/workshops/03-coercion-and-truthiness/03-my-or-my-and/my-or-my-and.js
+++ bootcamp-prep-master/workshops/03-coercion-and-truthiness/03-my-or-my-and/my-or-my-and.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+
+const myOr = (arg1, arg2, arg3) => {
+    if(!!arg1){
+      return arg1
+    }
+    if(!!arg2){
+      return arg2
+    }
+    // if(!!arg3){
+    //   return arg3
+    // }
+    return arg3
+  }
+  
