08469a8433c27f3b091056603d5d61c34fc0adf2
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-07 12:16:59 -0500

    implement isTruthy

--- bootcamp-prep-master/workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
+++ bootcamp-prep-master/workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
@@ -1,1 +1,22 @@
 // YOUR CODE BELOW
+
+const isTruthy = (arg) => {
+    if(arg === false){
+      return 'The boolean value false is falsey'
+    } 
+    if( arg === null){
+      return 'The null value is falsey'
+    } 
+    if( arg === undefined){
+      return 'undefined is falsey'
+    } 
+    if(arg === 0){
+      return `The number 0 is falsey (the only falsey number)`
+    } 
+    if (arg === ''){
+      return `The empty string is falsey (the only falsey string)`
+    } 
+    if(!!arg){
+      return true
+    }
+  }
