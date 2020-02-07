37788803d896f8cda649bf25c08278da208d8b16
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-07 12:18:35 -0500

    implementation for myAnd

--- bootcamp-prep-master/workshops/03-coercion-and-truthiness/03-my-or-my-and/my-or-my-and.js
+++ bootcamp-prep-master/workshops/03-coercion-and-truthiness/03-my-or-my-and/my-or-my-and.js
@@ -12,4 +12,20 @@ const myOr = (arg1, arg2, arg3) => {
     // }
     return arg3
   }
+
+  
+
+const myAnd = (arg1, arg2, arg3) => {
+    if(!arg1){
+      return arg1
+    } 
+    if(!arg2){
+      return arg2
+    }
+    if(!arg3){
+      return arg3
+    } else {
+      return 'yay!'
+    }
+  }
   
