2829d554ade998acf8d3e71930c715664001ca23
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-08 15:26:27 -0500

    implementation of mySlice

--- bootcamp-prep-master/workshops/02-loops/05-my-slice/my-slice.js
+++ bootcamp-prep-master/workshops/02-loops/05-my-slice/my-slice.js
@@ -1,1 +1,19 @@
 // YOUR CODE BELOW
+
+const mySlice = (originalString, startIdx, endIdx) => {
+    let output = ''
+    if(!startIdx && !endIdx){
+      return originalString
+    }
+    if(!endIdx){
+      for(let i = startIdx; i < originalString.length; i++){
+        output += originalString[i]
+      }
+    }
+     else {
+       for(let i = startIdx; i < endIdx; i++){
+         output += originalString[i]
+       }
+     }
+    return output
+  }
