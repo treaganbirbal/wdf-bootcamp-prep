76408439e6eb09e23ba0266e011c7e73020bcf93
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-08 15:18:59 -0500

    implementation of one & only

--- bootcamp-prep-master/workshops/03-coercion-and-truthiness/04-only-one/only-one.js
+++ bootcamp-prep-master/workshops/03-coercion-and-truthiness/04-only-one/only-one.js
@@ -1,1 +1,25 @@
 // YOUR CODE BELOW
+const onlyOne = (arg1, arg2, arg3) => {
+    if(!!arg1 && !!arg2 && !!arg3){
+      return false
+    }
+    if(!arg1 && !arg2 && arg3){
+      return true
+    }
+    if(!arg1 && arg2 && !arg3){
+      return true
+    } if(arg1 && !arg2 && !arg3){
+      return true
+    } if(!arg1 && !arg2 && !arg3){
+      return false
+    } if(!arg1 && arg2 && arg3){
+      return false
+    } 
+    if(arg1 && !arg2 && arg3){
+      return false
+    }
+    if(arg1 && arg2 && !arg3){
+      return false
+    }
+  }
+  
