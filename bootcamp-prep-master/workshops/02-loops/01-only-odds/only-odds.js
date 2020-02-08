d25e1fe8dc425e785701a3c1797a2ec8bb3c2c0b
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-08 15:22:32 -0500

    implementation of onlyOdds

--- bootcamp-prep-master/workshops/02-loops/01-only-odds/only-odds.js
+++ bootcamp-prep-master/workshops/02-loops/01-only-odds/only-odds.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+
+const onlyOdds = (num) => {
+    let sum = 0;
+    if(num < 1){
+      return 0
+    }
+    for(let i = 0 ; i <= num; i++){
+      if(i % 2 === 1){
+        sum = sum + i
+      }
+    }
+    return sum
+  }
+  
