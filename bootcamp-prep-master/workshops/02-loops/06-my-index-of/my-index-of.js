02f73cc28b4466f9ace49a611ef101cf7ec17a87
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-08 15:27:41 -0500

    implementation of myIndexOf

--- bootcamp-prep-master/workshops/02-loops/06-my-index-of/my-index-of.js
+++ bootcamp-prep-master/workshops/02-loops/06-my-index-of/my-index-of.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+
+const myIndexOf = (source, searchValue, startIdx=0) => {
+    for(let i = startIdx; i <= source.length - searchValue.length; i++){
+      let subStr = source.slice(i, i + searchValue.length);
+      
+      if(subStr === searchValue){
+        return i
+      }
+    }
+    return -1
+  }
