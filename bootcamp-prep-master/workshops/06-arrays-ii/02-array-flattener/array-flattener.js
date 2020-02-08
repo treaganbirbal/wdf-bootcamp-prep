faec2a5d0c0babed6079a12977ba11ed219cb4b4
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-08 15:36:28 -0500

    implemenation of arrayFlattener

--- bootcamp-prep-master/workshops/06-arrays-ii/02-array-flattener/array-flattener.js
+++ bootcamp-prep-master/workshops/06-arrays-ii/02-array-flattener/array-flattener.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+
+function arrayFlattener(array2d){
+    let oned = [];
+    for(let i = 0; i < array2d.length; i++){
+      if(Array.isArray(array2d[i])){
+        for(let j = 0; j < array2d[i].length; j++){
+          oned.push(array2d[i][j]);
+        }
+      }
+    else{
+      oned.push(array2d[i]);
+    }
+    }
+    // returns a new one-d array
+    return oned;
+  }
