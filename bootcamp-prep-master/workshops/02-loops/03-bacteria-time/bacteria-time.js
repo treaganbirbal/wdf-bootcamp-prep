7dfe590178e41472841b8b48a078af31e01c7299
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-08 15:24:54 -0500

    implementation of bacteriaTime

--- bootcamp-prep-master/workshops/02-loops/03-bacteria-time/bacteria-time.js
+++ bootcamp-prep-master/workshops/02-loops/03-bacteria-time/bacteria-time.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+
+const bacteriaTime = (currentNum, targetNum) => {
+    let numOfMins = 0
+    if(targetNum < currentNum){
+      return 'targetNum must be larger than currentNum'
+    } 
+    while(targetNum > currentNum){
+      currentNum *= 2
+      numOfMins +=20
+    }
+    return numOfMins
+  }
