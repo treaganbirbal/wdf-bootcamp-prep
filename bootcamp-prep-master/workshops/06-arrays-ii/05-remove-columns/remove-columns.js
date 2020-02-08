84b70329e1be0031afc8bd1b8c11449f5ff54809
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-08 15:39:38 -0500

    implementation of remove columns

--- bootcamp-prep-master/workshops/06-arrays-ii/05-remove-columns/remove-columns.js
+++ bootcamp-prep-master/workshops/06-arrays-ii/05-remove-columns/remove-columns.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+
+const removeColumns = (grid, numOfColumns) => {
+    for(let i = 0; i < grid.length; i++){
+      for(let j = 0; j < numOfColumns; j++){
+        grid[i].pop()
+      }
+    }
+    return grid
+  }
