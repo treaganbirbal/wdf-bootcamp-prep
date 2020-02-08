f4fbfe030617e5484ba4bcaac3ae7d199b6529df
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-08 15:30:47 -0500

    implementation of myIncludes

--- bootcamp-prep-master/workshops/05-arrays-i/02-my-includes/my-includes.js
+++ bootcamp-prep-master/workshops/05-arrays-i/02-my-includes/my-includes.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+
+const myIncludes = (arr, searchValue) => {
+    for(let i = 0 ; i < arr.length; i++){
+      if(searchValue === arr[i]){
+        return true
+      }
+    }
+    return false
+  }
+  
