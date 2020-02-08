80398970328d7c9e5c5bd7099e1b2a65ba202432
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-08 15:38:31 -0500

    implementation of makeGrid

--- bootcamp-prep-master/workshops/06-arrays-ii/04-make-grid/make-grid.js
+++ bootcamp-prep-master/workshops/06-arrays-ii/04-make-grid/make-grid.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+
+function makeGrid(numColumns, numRows){
+    let grid = [];
+    let row = [];
+    for(let i = 0; i < numColumns; i++){
+      row.push(i+1);
+    }
+    for(let j = 0; j < numRows; j++){
+      grid.push(row);
+    }  
+    
+    //return a two-dimensional array that represents a grid of the given dimensions.
+    return grid;
+  }
+  
