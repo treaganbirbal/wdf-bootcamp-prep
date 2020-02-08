997dfc55ecabb892f2054bfae941ba7cdec2cb2f
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-08 15:37:31 -0500

    implementation of zooInventory

--- bootcamp-prep-master/workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
+++ bootcamp-prep-master/workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
@@ -1,1 +1,19 @@
 // YOUR CODE BELOW
+
+const zooInventory = (multiArr) => {
+    const output = [];
+    
+    for(let i =0; i < multiArr.length; i++){
+        let sentence = '';
+  
+      let name = multiArr[i][0]
+      let animalType = multiArr[i][1][0]
+      let age = multiArr[i][1][1]
+      sentence += name + ' the ' + animalType + ' is ' +        age + '.' 
+      output.push(sentence)
+      // sentence = ''
+    }
+    
+    //return a 1d array with each element is a sentence of the animals
+    return output
+  }
