ca472e25430f5bcfcab6a1d31876ba64b1d06e37
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-08 15:30:03 -0500

    implementation of oddCouple

--- bootcamp-prep-master/workshops/05-arrays-i/01-odd-couple/odd-couple.js
+++ bootcamp-prep-master/workshops/05-arrays-i/01-odd-couple/odd-couple.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+
+const oddCouple = (arr) => {
+    let oddNumArr = []
+    for(let i = 0; i < arr.length; i++){
+      if(arr[i] % 2 === 1){
+        oddNumArr.push(arr[i])
+      }
+    }
+    if(oddNumArr.length > 2){
+      return oddNumArr.slice(0, 2)
+    }
+    return oddNumArr
+  }
+  
