0ae5374dac609ea61cfb99d4542a8bf538705bf1
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-08 15:41:24 -0500

    implemenation of mySlice

--- bootcamp-prep-master/workshops/06-arrays-ii/07-my-slice/my-slice.js
+++ bootcamp-prep-master/workshops/06-arrays-ii/07-my-slice/my-slice.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+
+const mySlice = (arr, startIdx = 0, endIdx = arr.length ) => {
+    const output = [];
+    
+    if(startIdx < 0) startIdx = startIdx + arr.length;
+    if(endIdx < 0) endIdx = endIdx + arr.length
+    
+    for(let i = startIdx; i < endIdx; ++i){
+      let currentEl = arr[i]
+      output.push(currentEl)
+  }
+    return output
+  }
