f3e03f0d650e19d56edff8714ae7bc37104796e0
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-08 15:40:24 -0500

    implementation of myJoin

--- bootcamp-prep-master/workshops/06-arrays-ii/06-my-join/my-join.js
+++ bootcamp-prep-master/workshops/06-arrays-ii/06-my-join/my-join.js
@@ -1,1 +1,18 @@
 // YOUR CODE BELOW
+
+function myJoin(array, seperator=","){
+    let acc = "";
+    for(let i = 0; i < array.length; i++){
+      if(array[i] === undefined || array[i] === null){
+        acc = acc + seperator
+        continue
+      }
+      if(i === array.length - 1){
+        acc = acc + array[i];
+      }
+      else{
+        acc = acc + array[i] + seperator;
+      }
+    }
+    return acc;
+  }
