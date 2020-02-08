3337c5f7f8eef746f87823e09771f978a0f0a206
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-08 15:23:51 -0500

    implementation of crazyCaps

--- bootcamp-prep-master/workshops/02-loops/02-crazy-caps/crazy-caps.js
+++ bootcamp-prep-master/workshops/02-loops/02-crazy-caps/crazy-caps.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+
+const crazyCaps = (str) => {
+    let output = '';
+    for(let i = 0; i < str.length; i++){
+      if(i % 2 === 1){
+        output += str[i].toUpperCase()
+      }
+      if(i % 2 === 0){
+        output += str[i].toLowerCase()
+      }
+    }
+    return output
+  }
