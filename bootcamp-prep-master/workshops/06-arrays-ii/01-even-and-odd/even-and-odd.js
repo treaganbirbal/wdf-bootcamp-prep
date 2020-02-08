60fc8f4d7fcc79eee2c9bb6129efe9d73463259b
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-08 15:35:27 -0500

    implementation of even & odds

--- bootcamp-prep-master/workshops/06-arrays-ii/01-even-and-odd/even-and-odd.js
+++ bootcamp-prep-master/workshops/06-arrays-ii/01-even-and-odd/even-and-odd.js
@@ -1,1 +1,18 @@
 // YOUR CODE BELOW
+
+const evenAndOdd = (arr) => {
+    const output = [];
+    const evenArr = [];
+    const oddArr = [];
+    for(let i = 0; i < arr.length; i++){
+      if(arr[i] % 2){
+        oddArr.push(arr[i])
+      } else {
+        evenArr.push(arr[i])
+      }
+    }
+    output.push(evenArr, oddArr)
+    return output
+    
+  }
+  
