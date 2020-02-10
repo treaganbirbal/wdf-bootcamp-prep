4d22717e5adf4dc3dd15ad3cf4c3c4acf1160092
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-09 23:41:53 -0500

    implement condition to check bottomCenter subGrid

--- Projects/sudoku_checker/app.js
+++ Projects/sudoku_checker/app.js
@@ -113,7 +113,7 @@ const getGrid = (grid, xCoordinate, yCoordinate) => {
         midCenter = subGrid.slice(0,3).flat(1)
         return midCenter
     }
-    if(xCoordinate === 1 && yCoordinate === 2){
+    if(xCoordinate === 2 && yCoordinate === 1){
         for(let i = 3; i < grid.length; i++){
             subGrid.push(grid[i].slice(6, 9))
             console.log(subGrid)
@@ -129,12 +129,21 @@ const getGrid = (grid, xCoordinate, yCoordinate) => {
         bottomLeft = subGrid.slice(0,3).flat(1)
         return bottomLeft
     }
+    if(xCoordinate === 1 && yCoordinate === 2){
+        for(let i = 6; i < grid.length; i++){
+            subGrid.push(grid[i].slice(3, 6))
+            console.log(subGrid)
+        }
+        bottomCenter = subGrid.slice(0,3).flat(1)
+        return bottomCenter
+    }
+    
 // console.log(topLeft)
 }
 
 
 
-console.log(getGrid(puzzle, 0, 2 )) // -> [ 7,4,2,9,6,3,5,8,1 ]
+console.log(getGrid(puzzle, 1, 2 )) // -> [ 7,4,2,9,6,3,5,8,1 ]
 
 
 
