66ca7f5c329cdbd2a9b83031f0d6d8a938f0cbe5
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-09 23:31:03 -0500

    currently implmenting getGrid method. Already heave 5 subGrids

--- Projects/sudoku_checker/app.js
+++ Projects/sudoku_checker/app.js
@@ -1,16 +1,16 @@
-let puzzle = [
-  [8, 9, 5, 7, 4, 2, 1, 3, 6],
-  [2, 7, 1, 9, 6, 3, 4, 8, 5],
-  [4, 6, 3, 5, 8, 1, 7, 9, 2],
+// let puzzle = [
+//   [8, 9, 5,   7, 4, 2,   1, 3, 6],
+//   [2, 7, 1,   9, 6, 3,   4, 8, 5],
+//   [4, 6, 3,   5, 8, 1,   7, 9, 2],
 
-  [9, 3, 4, 6, 1, 7, 2, 5, 8],
-  [5, 1, 7, 2, 3, 8, 9, 6, 4],
-  [6, 8, 2, 4, 5, 9, 3, 7, 1],
+//   [9, 3, 4,   6, 1, 7,   2, 5, 8],
+//   [5, 1, 7,   2, 3, 8,   9, 6, 4],
+//   [6, 8, 2,   4, 5, 9,   3, 7, 1],
 
-  [1, 5, 9, 8, 7, 4, 6, 2, 3],
-  [7, 4, 6, 3, 2, 5, 8, 1, 9],
-  [3, 2, 8, 1, 9, 6, 5, 4, 7]
-];
+//   [1, 5, 9,   8, 7, 4,   6, 2, 3],
+//   [7, 4, 6,   3, 2, 5,   8, 1, 9],
+//   [3, 2, 8,   1, 9, 6,   5, 4, 7]
+// ];
 
 //-------getRow: This function should accept two arguments: a sudoku grid (represented by an array of arrays) and a row index. The function should return an array containing the numbers in the specified row.
 
@@ -48,9 +48,78 @@ const getColumn = (grid, columnIdx) => {
 
 //getGrid: This function should accept three arguments: a sudoku grid, and an x and y coordinate for one of the puzzle's 3x3 subgrids. The function should return an array with all the numbers in the specified subgrid.
 
+let puzzle = [
+    [8, 9, 5,   7, 4, 2,   1, 3, 6],
+    [2, 7, 1,   9, 6, 3,   4, 8, 5],
+    [4, 6, 3,   5, 8, 1,   7, 9, 2],
+  
+    [9, 3, 4,   6, 1, 7,   2, 5, 8],
+    [5, 1, 7,   2, 3, 8,   9, 6, 4],
+    [6, 8, 2,   4, 5, 9,   3, 7, 1],
+  
+    [1, 5, 9,   8, 7, 4,   6, 2, 3],
+    [7, 4, 6,   3, 2, 5,   8, 1, 9],
+    [3, 2, 8,   1, 9, 6,   5, 4, 7]
+  ];
+
 const getGrid = (grid, xCoordinate, yCoordinate) => {
-    
+    let topLeft
+    let topCenter
+    let topRight
+    let midleft
+    let midCenter
+    let midRight
+    let bottomLeft
+    let bottomCenter
+    let bottomRight
+    let subGrid= []
+  
+    if(xCoordinate === 0 && yCoordinate === 0){
+        for(let i = 0; i < grid.length; i++){
+            subGrid.push(grid[i].slice(0,3))
+        }
+        topLeft = subGrid.slice(0,3).flat(1)
+        return topLeft
+    }
+    if(xCoordinate === 1 && yCoordinate === 0){
+        for(let i = 0; i < grid.length; i++){
+            subGrid.push(grid[i].slice(3, 6))
+            console.log(subGrid)
+        }
+        topMiddle = subGrid.slice(0,3).flat(1)
+        return topMiddle
+    }
+    if(xCoordinate === 2 && yCoordinate === 0){
+        for(let i = 0; i < grid.length; i++){
+            subGrid.push(grid[i].slice(6, 9))
+            console.log(subGrid)
+        }
+        topRight = subGrid.slice(0,3).flat(1)
+        return topRight
+    }
+    if(xCoordinate === 0 && yCoordinate === 1){
+        for(let i = 3; i < grid.length; i++){
+            subGrid.push(grid[i].slice(0, 3))
+            console.log(subGrid)
+        }
+        midLeft = subGrid.slice(0,3).flat(1)
+        return midLeft
+    }
+    if(xCoordinate === 1 && yCoordinate === 1){
+        for(let i = 3; i < grid.length; i++){
+            subGrid.push(grid[i].slice(3, 6))
+            console.log(subGrid)
+        }
+        midCenter = subGrid.slice(0,3).flat(1)
+        return midCenter
+    }
+// console.log(topLeft)
 }
 
-// const getGrid()
+
+
+console.log(getGrid(puzzle, 1, 1 )) // -> [ 7,4,2,9,6,3,5,8,1 ]
+
+
+
 
