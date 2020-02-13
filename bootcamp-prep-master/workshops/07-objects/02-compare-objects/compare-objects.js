4ede86d8f976b59dc4373826f6efe606f63ae73b
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-13 14:10:29 -0500

    implementation of compareObjects

--- bootcamp-prep-master/workshops/07-objects/02-compare-objects/compare-objects.js
+++ bootcamp-prep-master/workshops/07-objects/02-compare-objects/compare-objects.js
@@ -1,1 +1,20 @@
 // YOUR CODE BELOW
+
+const compareObjects = (obj1, obj2) => {
+    let obj1Keys = Object.keys(obj1)
+    let obj2Keys = Object.keys(obj2)
+    
+    
+    if(obj1Keys.length !== obj2Keys.length){
+        return false
+      }
+    else {
+    for(let key in obj1){
+      if(obj1[key] !== obj2[key]){
+        return false
+      }
+    }
+    
+  }
+    return true
+  }
