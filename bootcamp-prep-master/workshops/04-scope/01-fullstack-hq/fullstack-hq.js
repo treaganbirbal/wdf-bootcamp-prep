bca9d9813738a9fd5a418fd96d49ba435fd67960
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-13 12:48:41 -0500

    implementation of fullStackHQ

--- bootcamp-prep-master/workshops/04-scope/01-fullstack-hq/fullstack-hq.js
+++ bootcamp-prep-master/workshops/04-scope/01-fullstack-hq/fullstack-hq.js
@@ -1,19 +1,18 @@
-// Edit the code below
-let place = 'Planet Earth';
-
-function fullstackHQ(){
+let planet = 'Planet Earth';
+function fullstackHQ(country){
+  country = 'United States';
   return middleFunction('United States');
-
-  function middleFunction(place) {
+  function middleFunction(state) {
+    state = 'New York State'
     return innerFunction('New York State');
-
-    function innerFunction(place) {
-      place = 'New York City';
-      return innermostFunction();
-
-      function innermostFunction() {
-        return 'Fullstack HQ is at ' + place + ', ' + place + ', ' + place + ', ' + place;
+    function innerFunction(city) {
+       city = 'New York City';
+      return innermostFunction('New York City');
+      function innermostFunction(place) {
+        return 'Fullstack HQ is at ' + planet + ', ' + country + ', ' + state + ', ' + place;
       }
     }
   }
 }
+
+console.log(fullstackHQ())
