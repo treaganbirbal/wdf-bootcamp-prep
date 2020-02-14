bf2ab7b2d1dab037534b0af14cd2e03cbd4a8bca
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-14 16:19:23 -0500

    implementation of callThemAll

--- bootcamp-prep-master/workshops/08-object-methods/03-call-them-all/call-them-all.js
+++ bootcamp-prep-master/workshops/08-object-methods/03-call-them-all/call-them-all.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+
+function callThemAll(obj, val) {
+    let methodResults = []
+    for(let key in obj){
+       let currentVal = obj[key]
+       
+       if(typeof currentVal === 'function'){
+        let methodResult =  currentVal(val)
+        methodResults.push(methodResult)
+       }
+    }
+    return methodResults
+  }
