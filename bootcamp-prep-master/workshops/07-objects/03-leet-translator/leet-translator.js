763c32f22fcb146997240111f7a48cbf48471b09
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-14 15:07:12 -0500

    start implmentation of leetTranslator func

--- bootcamp-prep-master/workshops/07-objects/03-leet-translator/leet-translator.js
+++ bootcamp-prep-master/workshops/07-objects/03-leet-translator/leet-translator.js
@@ -2,3 +2,7 @@ let letters = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 let leetChars = ['@', '8', '(', '|)', '3', 'ph', 'g', '#','l', '_|', '|<', '1', "|'|'|", '/\/', '0', '|D', '(,)', '|2', '5', '+', '|_|', '|/', "|/|/'",'><', 'j', '2'];
 
 // YOUR CODE BELOW
+
+function leetTranslator(obj1, obj2){
+
+}
