70f5e5faa94fa6f672285052d637546be8a768a5
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-16 17:55:45 -0500

    implmenetatin of countVowels method

--- bootcamp-prep-master/workshops/12-recursion-i/04-count-vowels/count-vowels.js
+++ bootcamp-prep-master/workshops/12-recursion-i/04-count-vowels/count-vowels.js
@@ -2,6 +2,18 @@
 
 
 function countVowels(sentence){
+    if(sentence.length === 0){
+        return 0
+    }
     let count = 0;
-    return count
+    if(isAVowel(str[0])){
+        count += 1;
+    }
+    count += countVowels(sentence.slice(1))
+    return count;
   }
+
+function isAVowel(char){
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    return vowels.includes(char)
+}
