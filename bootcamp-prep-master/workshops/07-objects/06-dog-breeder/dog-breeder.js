7556eb6b3a6416553b936a144593cc13e0cd5e32
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-16 15:39:38 -0500

    add default

--- bootcamp-prep-master/workshops/07-objects/06-dog-breeder/dog-breeder.js
+++ bootcamp-prep-master/workshops/07-objects/06-dog-breeder/dog-breeder.js
@@ -5,6 +5,7 @@ function dogBreeder(name='Steve', age=0){
     if(!age){
         if(typeof name === 'number'){
         age = name;
+        name = 'Steve'
         }
     }
         dogObj['name'] = name;
