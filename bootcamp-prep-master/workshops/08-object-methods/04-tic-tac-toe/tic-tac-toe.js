c9caabb1e2464bfe29791ee7c3289a9e15dcb112
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-15 23:02:10 -0500

    start of tictactoe

--- bootcamp-prep-master/workshops/08-object-methods/04-tic-tac-toe/tic-tac-toe.js
+++ bootcamp-prep-master/workshops/08-object-methods/04-tic-tac-toe/tic-tac-toe.js
@@ -1,1 +1,25 @@
 // YOUR CODE BELOW
+
+const ticTacToe = {
+    //   board: 
+    //   [
+    //     [null, null, null],
+    //     [null, null, null],
+    //     [null, null, null]
+    //   ],
+      
+    //   move: function(player, x, y){
+    //     // for(let i = 0; i < this.board.length; i++){
+    //       // for(let j = 0; j < this.board[i].length; j++){
+    //     if(player === 'x'){
+    //         if( x === 0 && y === 0){
+    //           this.board[0][0] = 'x'
+    //         } 
+            
+    //      } 
+    //     return this.board
+    //   }
+      clear: function(board){
+        console.log(this.board)
+      }
+    }
