ddec5fb927dae618de771e78c4e8a36505de0579
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-16 16:37:35 -0500

    implementation of attendanceCheck

--- bootcamp-prep-master/workshops/07-objects/07-attendance-check/attendance-check.js
+++ bootcamp-prep-master/workshops/07-objects/07-attendance-check/attendance-check.js
@@ -41,6 +41,19 @@ let classRoom = [
 
 function attendanceCheck(day){
     const studentsPresent = [];
+
+    for(let i = 0; i < classRoom.length; i++){
+        let studentObj = classRoom[i];
+        let studentName = Object.keys(studentObj)[0];
+        let attendanceObjs = studentObj[studentName];
+        for(let j = 0; j < attendanceObjs.length; j++){
+            let attendanceObj = attendanceObjs[j];
+            let dayName = Object.keys(attendanceObj)[0]
+            if(dayName === day && attendanceObj[dayName]){
+                studentsPresent.push(studentName)
+            }
+        }
+    }
     
     return studentsPresent
   }
