39d3e2ed6ade2de34987b13e3dea06cf67919a71
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-16 15:22:04 -0500

    frequencyAnalyysis implementation

--- bootcamp-prep-master/workshops/07-objects/05-frequency-analysis/frequency-analysis.js
+++ bootcamp-prep-master/workshops/07-objects/05-frequency-analysis/frequency-analysis.js
@@ -2,9 +2,19 @@
 
 function frequencyAnalysis(str){
     const charMap = {}
-    for(let i = 0; i < str.length; i++){
-      
+    if(!str.length){
+      return
     }
+    for(let i = 0; i < str.length; i++){
+      let char = str[i]
+      if(!charMap[char]){
+        charMap[char] = 1
+      } else {
+        charMap[char]++
+      }
+    }
+    
+    console.log(charMap)
     return charMap
   }
   
