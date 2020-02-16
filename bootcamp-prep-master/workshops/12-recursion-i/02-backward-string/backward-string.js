c1733ef947ed1337d80753414361032d8a5fd8f1
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-16 17:35:22 -0500

    implementation of backwardsString

--- bootcamp-prep-master/workshops/12-recursion-i/02-backward-string/backward-string.js
+++ bootcamp-prep-master/workshops/12-recursion-i/02-backward-string/backward-string.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+
+function backwardStr(str){
+    if(str.length === 1){
+        console.log(str)
+    }
+    else {
+        let lastLetter = str[str.length - 1]
+        console.log(lastLetter)
+        str = str.slice(0, -1)
+        backwardStr(str)
+    }
+}
