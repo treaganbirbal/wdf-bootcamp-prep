3d95a6e7cfc10e9a0b04f816b5c618e93d52d57c
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-16 15:37:01 -0500

    check if there is no name then the arg is now age and name = age

--- bootcamp-prep-master/workshops/07-objects/06-dog-breeder/dog-breeder.js
+++ bootcamp-prep-master/workshops/07-objects/06-dog-breeder/dog-breeder.js
@@ -1,6 +1,12 @@
 // YOUR CODE BELOW
 
-function dogBreeder(name, age){
+function dogBreeder(name='Steve', age=0){
     const dogObj = {}
+    if(!age){
+        if(typeof name === 'number'){
+        age = name;
+        }
+    }
+
     return dogObj
   }
