23ffa6fb1328c5bd9000e122e4d7877f6f21bbfb
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-16 21:53:09 -0500

    implementation of sumNums

--- bootcamp-prep-master/workshops/12-recursion-i/06-sum-digits/sum-digits.js
+++ bootcamp-prep-master/workshops/12-recursion-i/06-sum-digits/sum-digits.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+
+const sumDigits = (num) => {
+    let str = num.toString()
+    if(str.length === 1){
+        return num
+    }
+    let sum = 0;
+    sum += Number(str[0])
+    sum += sumDigits(Number(str.slice(1)))
+    // console.log(sum)
+    return sum
+}
+
+console.log(sumDigits(1234))
