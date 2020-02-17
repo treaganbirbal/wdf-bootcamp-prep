531b86ac890f5f1af32b3b589111f28ea25abe46
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-16 21:39:27 -0500

    reverseArray func implmementation

--- bootcamp-prep-master/workshops/12-recursion-i/05-reverse-array/reverse-array.js
+++ bootcamp-prep-master/workshops/12-recursion-i/05-reverse-array/reverse-array.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+
+const reverseArray = (array) => {
+    if(array.length === 1){
+        return array.slice()
+    }
+    const newArr = []
+    let lastEl = array[array.length - 1]
+    newArr.push(lastEl)
+    let remainingEls = array.slice(0, -1)
+    newArr = newArr.concat(remainingEls)
+
+    return newArr
+}
