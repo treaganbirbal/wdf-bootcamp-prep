3d42d7b6955a3b572bba495fec78507f20025b9d
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-16 22:23:31 -0500

    implmenetation of isPalindrome recursive solution

--- bootcamp-prep-master/workshops/12-recursion-i/07-is-palindrome/is-palindrome.js
+++ bootcamp-prep-master/workshops/12-recursion-i/07-is-palindrome/is-palindrome.js
@@ -1,1 +1,20 @@
 // YOUR CODE BELOW
+const isPalindrome = (str) => {
+    str = str.toLowerCase();
+    if(str.length === 1){
+        return true
+    }
+    if(str.length === 2){
+        return str[0] === str[1]
+    }
+    let firstLetter = str[0];
+    let lastLetter = str.slice(-1);
+
+    if(firstLetter !== lastLetter){
+        return false
+    }
+
+    let remainingStr = str.slice(1, -1)
+    return isPalindrome(remainingStr)
+}
+
