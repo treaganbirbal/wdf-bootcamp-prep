438a29971e530b1d17b3e3eadf18a0f38953c61c
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-16 21:43:29 -0500

    bug fix of where to make recursive call

--- bootcamp-prep-master/workshops/12-recursion-i/05-reverse-array/reverse-array.js
+++ bootcamp-prep-master/workshops/12-recursion-i/05-reverse-array/reverse-array.js
@@ -4,11 +4,13 @@ const reverseArray = (array) => {
     if(array.length === 1){
         return array.slice()
     }
-    const newArr = []
+    let newArr = []
     let lastEl = array[array.length - 1]
     newArr.push(lastEl)
-    let remainingEls = array.slice(0, -1)
-    newArr = newArr.concat(remainingEls)
+    let reverseRemainingEls = reverseArray(array.slice(0, -1))
+    newArr = newArr.concat(reverseRemainingEls)
 
     return newArr
 }
+
+console.log(reverseArray([1,2,3]))
