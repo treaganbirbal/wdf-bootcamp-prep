ce0f04c760362ba789fb64682aab4a599d7f9a0f
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-18 12:37:04 -0500

    longestName implementation

--- bootcamp-prep-master/workshops/13-recursion-ii/04-get-longest-name/get-longest-name.js
+++ bootcamp-prep-master/workshops/13-recursion-ii/04-get-longest-name/get-longest-name.js
@@ -3,5 +3,17 @@
 function getLongestName(family){
     let longestName = '';
 
+    for(let name in family){
+        if(name.length > longestName.length){
+            longestName = name;
+        }
+        if(family[name]){
+            let decentantsLongstName = getLongestName(family[name])
+            if(decentantsLongstName > longestName){
+                longestName = decentantsLongstName
+            }
+        }
+    }
+
     return longestName
 }
