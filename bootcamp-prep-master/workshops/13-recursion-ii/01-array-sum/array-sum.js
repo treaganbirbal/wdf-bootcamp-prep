02b79626ce9dfffdff8201093fbb7c48de03f89d
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-18 12:19:51 -0500

    implementation of arraySum

--- bootcamp-prep-master/workshops/13-recursion-ii/01-array-sum/array-sum.js
+++ bootcamp-prep-master/workshops/13-recursion-ii/01-array-sum/array-sum.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+
+const arraySum = (arr) => {
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++){
+        let element = arr[i];
+        if(Array.isArray(element)){
+            sum += arraySum(element)
+        } else {
+            sum += element
+        }
+    }
+    return sum
+}
+
+console.log(arraySum([[10], [10], [10]]))
