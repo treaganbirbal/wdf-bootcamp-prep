ed622bcfa5f2b66ebf634b73169d62f923e2f153
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-18 12:23:31 -0500

    implementation of allSystemsGo

--- bootcamp-prep-master/workshops/13-recursion-ii/02-all-systems-go/all-systems-go.js
+++ bootcamp-prep-master/workshops/13-recursion-ii/02-all-systems-go/all-systems-go.js
@@ -1,1 +1,29 @@
 // YOUR CODE BELOW
+
+function allSystemsGo(systems){
+    for(let keys in systems){
+        if(typeof(systems[keys]) === 'object'){
+            let isValid = allSystemsGo(systems[keys])
+            if(!isValid){
+                return false
+            }
+        } else if(!systems[keys]){
+            return false
+        }
+    }
+    return true;
+}
+
+function allSystemsGo(anObj){
+    for(let keys in anObj){
+      if(typeof(anObj[keys]) === 'object'){
+        let isValid = allSystemsGo(anObj[keys])
+        if(!isValid){
+          return false
+        } 
+      } else if(!anObj[keys]){
+        return false
+      }
+    }
+    return true;
+  }
