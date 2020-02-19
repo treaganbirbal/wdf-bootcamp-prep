6b1ca33d451bdfd2b016fe972930592738e33db5
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-19 13:09:28 -0500

    implementation of searchParty

--- bootcamp-prep-master/workshops/13-recursion-ii/05-search-party/search-party.js
+++ bootcamp-prep-master/workshops/13-recursion-ii/05-search-party/search-party.js
@@ -1,5 +1,26 @@
 // YOUR CODE BELOW
 
-function searchParty(){
+function searchParty(name, worldObj){
+
+    for(let place in worldObj){
+        if(typeof(worldObj[place] === 'string')){
+            return [place]
+        }
+        else if(Array.isArray(worldObj[place])){
+            let people = worldObj[place];
+            for( let i = 0; i < people.length; i++){
+                if(name === people[i]){
+                    return [place]
+                }
+            }
+        } else {
+            let nextPlace = worldObj[place];
+            let resultFromNextPlace = searchParty(name, nextPlace);
+            if(resultFromNextPlace){
+            return [place].concat(resultFromNextPlace)
+            }
+        }
+    }
     
+    return null;
 }
