841fea67793aacc79a90be97ea63ab7de24c044e
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-19 13:36:47 -0500

    implmentation of zeroDarkThirty func

--- bootcamp-prep-master/workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
+++ bootcamp-prep-master/workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+
+function zeroDarkThirty(num){
+    let numsArr = num.toString().split('')
+    
+    let noZerosArr = numsArr.filter(el => {
+      return el !== '0'
+    })
+    let strOfNums = noZerosArr.join('');
+    if(strOfNums.length === 0){
+      return NaN
+    }
+    return Number(strOfNums)
+  }
+  
