8ff8c01af1fa9d9c87ef0a07a61abe0a027bef61
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-20 14:03:20 -0500

    implementation of billerBuilder func

--- bootcamp-prep-master/workshops/11-higher-order-functions-ii/01-biller-builder/biller-builder.js
+++ bootcamp-prep-master/workshops/11-higher-order-functions-ii/01-biller-builder/biller-builder.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+
+const billerBuilder = (state) => {
+    if(state === 'NY'){
+        return function (price) {
+            if(state === 'NY'){
+                return price * 1.03 * 1.04
+            }
+        }
+    } 
+    if(state === 'NJ'){
+        return function (price){
+            return price * 1.05 * 1.06625
+        }
+    }
+}
+
