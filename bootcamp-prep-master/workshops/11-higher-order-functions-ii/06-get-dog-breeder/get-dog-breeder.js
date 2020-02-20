b1b6c9b9e8573169447d93745cc5f98acde038e7
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-20 14:41:54 -0500

    implementation of getDogBreeder

--- bootcamp-prep-master/workshops/11-higher-order-functions-ii/06-get-dog-breeder/get-dog-breeder.js
+++ bootcamp-prep-master/workshops/11-higher-order-functions-ii/06-get-dog-breeder/get-dog-breeder.js
@@ -1,2 +1,21 @@
 // YOUR CODE BELOW
 
+const getDogBreeder = (defaultName, defaultAge) => {
+  
+  
+    return function dogBreeder(name=defaultName, age=defaultAge) {
+    if (typeof name === 'number') {
+      age = name;
+      name = defaultName
+    }
+  
+    let newDog =  {
+      name: name,
+      age: age
+    };
+  
+    return newDog;
+  }
+    
+    
+  }
